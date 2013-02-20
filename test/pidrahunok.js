cbf8b4bc8df21ee9205e56ba3092ad4bdd0f5c59
Oleksandr Khomenko <okhomenko@gmail.com>
2013-02-20 08:59:35 +0200

    Images

--- test/pidrahunok.js
+++ test/pidrahunok.js
@@ -12,31 +12,14 @@ function renderMentions (mentions, candidate) {
 function renderResults (yu, ya) {
   $('.score.yu').text(yu);
   $('.score.ya').text(ya);
-
-  if (yu > ya) {
-    $('#yu').addClass('winner');
-    $('#ya').addClass('looser');
-  }
-  if (yu < ya) {
-    $('#ya').addClass('winner');
-    $('#yu').addClass('looser');
-  }
 }
 
-$.when(
-  getMentions('yu'),
-  getMentions('ya')
-).done(function (data1, data2) {
-  var yu = data1[0], ya = data2[0];
+$.when(getMentions ('yu'), getMentions('ya'))
+  .done(function (data1, data2) {
+    var yu = data1[0], ya = data2[0];
 
-  if (yu.length > ya.length) { yu.length = ya.length - 1; }
+    renderMentions(ya, 'ya');
+    renderMentions(yu, 'yu');
 
-  renderMentions(yu, 'yu');
-  renderMentions(ya, 'ya');
-
-  renderResults(yu.length, ya.length)
-}).fail(function () {
-  $('#ya, #yu').addClass('looser small');
-
-  $('.hooligans').fadeIn();
-});
+    renderResults(yu, ya);
+  })
